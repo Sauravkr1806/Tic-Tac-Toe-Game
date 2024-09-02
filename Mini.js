107c50769e2236b44e6c2358b803588baab203b3
Saurav Kumar <136987475+Sauravkr1806@users.noreply.github.com>
2024-09-02 20:56:29 +0530

    Update Mini.js

--- Mini.js
+++ Mini.js
@@ -3,11 +3,11 @@ let btn = document.querySelector(".btn");
 let btn1 = document.querySelector(".btn1");
 let msgContainer = document.querySelector(".msgContainer");
 let para = document.querySelector("#para");
-let Gametime = new Audio("Background.mp3");
-let Gamedraw = new Audio("Drawgame.mp3");
-let GameWinner = new Audio("Winner.mp3");
-let turn1 = new Audio("move1.mp3");
-let turn2 = new Audio("move2.mp3");
+let Gametime = new Audio("./Sounds/Background.mp3");
+let Gamedraw = new Audio("./Sounds/Drawgame.mp3");
+let GameWinner = new Audio("./Sounds/Winner.mp3");
+let turn1 = new Audio("./Sounds/move1.mp3");
+let turn2 = new Audio("./Sounds/move2.mp3");
 
 let turn0 = true;
 let turn = "X";
